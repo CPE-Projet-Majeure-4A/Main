30e423a2c6b56bbc9bd4044831d54bad33b2bc46
timothee-l <tim.levilly@gmail.com>
2022-06-01 09:57:27 +0200

    Init projet Springboot

---
+++ src/main/java/com/majeur/projet/ProjetMajeurApplication.java
@@ -0,0 +1,13 @@
+package com.majeur.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMajeurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetMajeurApplication.class, args);
+    }
+
+}
