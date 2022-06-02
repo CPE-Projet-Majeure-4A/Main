5afa8c5af7531972eba96a2fc504a6caefab6873
Tancrède Maillot <75097976+Tanma69@users.noreply.github.com>
2022-06-02 11:31:44 +0200

    Update SendVehicle.java

--- src/main/java/com/majeur/projet/Updater/Vehicle/SendVehicle.java
+++ src/main/java/com/majeur/projet/Updater/Vehicle/SendVehicle.java
@@ -12,4 +12,28 @@ package com.majeur.projet.Updater.Vehicle;
  * findDestination(vehicleId, listFire)
  */
 public class SendVehicle {
+  
+/*
+ * Method : GetFireInArea , require all fires in a list and facility coord
+ * check distance and if distance greater than 50 delete fire from list
+ * return listFire
+ */
+	
+/*
+* Called for every vehicle
+* Method : SelectVehicle , require listFire and a vehicle
+* launches WeightFunction -> return fireId (the one with the highest weight)
+* launches sendVehicleToFire(vehicleId, fireId) or sendVehicleToFacility(vehicleId)
+* return void
+*/
+
+	
+/*
+* Method : WeightFunction(listFire, vehicle)
+* Distance vehicle-fire
+* Agent vs Fire Type
+* Fire range and intensity vs vehicle Capacity
+* Find the highest weight
+* return fireId (from the fire with highest weight)
+*/
 }
